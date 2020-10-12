4886a2c021ddb872d5f78b18241eb18be93b4b63
ryanechternacht <ryan@echternacht.org>
2020-10-12 16:17:16 -0400

    hookup with mount (for starting/stopping)

---
+++ src/yardstick/scheduler.clj
@@ -0,0 +1,46 @@
+(ns yardstick.scheduler
+  (:require [mount.core :as mount]
+            [yardstick.process-job :as pj]
+            [yardstick.jobs.sample-students :as ss])
+  (:import
+   (java.util.concurrent Executors ScheduledExecutorService ThreadFactory TimeUnit)))
+
+(defn ^ThreadFactory create-thread-factory
+  [thread-name-prefix]
+  (let [thread-number (atom 0)]
+    (reify ThreadFactory
+      (newThread [_ runnable]
+        (Thread. runnable (str thread-name-prefix "-" (swap! thread-number inc)))))))
+
+(defn ^ScheduledExecutorService create-single-thread-scheduled-executor
+  [thread-name-prefix]
+  (let [thread-factory (create-thread-factory thread-name-prefix)]
+    (Executors/newSingleThreadScheduledExecutor thread-factory)))
+
+(defn schedule [executor runnable interval unit]
+  (.scheduleWithFixedDelay executor runnable 0 interval unit))
+
+(defn shutdown-executor
+  "Industrial-strength executor shutdown, modify/simplify according to need."
+  [^ScheduledExecutorService executor]
+  (.shutdown executor)
+  (if-not (.awaitTermination executor 10 TimeUnit/SECONDS)
+    (do
+      (.shutdownNow executor)
+      (prn "Still waiting to shut down executor. Sending interrupt to tasks.")
+      (when-not (.awaitTermination executor 10 TimeUnit/SECONDS)
+        (throw (ex-info "Executor could not be shut down" {}))))
+    (prn "Executor shutdown completed")))
+
+(defn- run-job
+  [ds]
+  (pj/run-job ss/job "resources/sample-students.csv" 1 ds))
+
+(def executor (create-single-thread-scheduled-executor "world-listener"))
+
+(defn mount-state
+  [ds]
+  (mount/defstate job-manager
+    :start (doto (create-single-thread-scheduled-executor "job-manager")
+             (schedule #(run-job ds) 10 TimeUnit/SECONDS))
+    :stop (shutdown-executor job-manager)))
--- src/yardstick/yardstick_jobs.clj
+++ src/yardstick/yardstick_jobs.clj
@@ -1,12 +1,9 @@
 (ns yardstick.yardstick-jobs
   (:gen-class)
-  (:require [next.jdbc :as jdbc]
-            [yardstick.jobs.sample-students :as ss]
-            [yardstick.process-job :as pj]
-            ;; [mount.core :as mount]
-            )
-  (:import
-   (java.util.concurrent Executors ScheduledExecutorService ThreadFactory TimeUnit)))
+  (:require
+   [mount.core :as mount]
+   [yardstick.scheduler :as ys]
+   [next.jdbc :as jdbc]))
 
 (defn -main
   "I don't do a whole lot ... yet."
@@ -22,57 +19,11 @@
 
 (def ds (jdbc/get-datasource db-conn))
 
-;; (pj/run-job ss/job "resources/sample-students.csv" 1 ds)
+(ys/mount-state ds)
 
-(defn ^ThreadFactory create-thread-factory
-  [thread-name-prefix]
-  (let [thread-number (atom 0)]
-    (reify ThreadFactory
-      (newThread [_ runnable]
-        (Thread. runnable (str thread-name-prefix "-" (swap! thread-number inc)))))))
+(mount/start)
 
-(defn ^ScheduledExecutorService create-single-thread-scheduled-executor
-  [thread-name-prefix]
-  (let [thread-factory (create-thread-factory thread-name-prefix)]
-    (Executors/newSingleThreadScheduledExecutor thread-factory)))
-
-(defn schedule [executor runnable interval unit]
-  (.scheduleWithFixedDelay executor runnable 0 interval unit))
-
-(defn shutdown-executor
-  "Industrial-strength executor shutdown, modify/simplify according to need."
-  [^ScheduledExecutorService executor]
-  (if (.isShutdown executor)
-    (prn "Executor already shut down")
-    (do
-      (prn "Shutting down executor")
-      (.shutdown executor)                                  ;; Disable new tasks from being scheduled
-      (try
-        ;; Wait a while for currently running tasks to finish
-        (if-not (.awaitTermination executor 10 TimeUnit/SECONDS)
-          (do
-            (.shutdownNow executor)                         ;; Cancel currently running tasks
-            (prn "Still waiting to shut down executor. Sending interrupt to tasks.")
-            ;; Wait a while for tasks to respond to being cancelled
-            (when-not (.awaitTermination executor 10 TimeUnit/SECONDS)
-              (throw (ex-info "Executor could not be shut down" {}))))
-          (prn "Executor shutdown completed"))
-        (catch InterruptedException _
-          (prn "Interrupted while shutting down. Sending interrupt to tasks.")
-          ;; Re-cancel if current thread also interrupted
-          (.shutdownNow executor)
-          ;; Preserve interrupt status
-          (.interrupt (Thread/currentThread)))))))
-
-(defn run-job
-  []
-  (pj/run-job ss/job "resources/sample-students.csv" 1 ds))
-
-(def executor (create-single-thread-scheduled-executor "world-listener"))
-
-(schedule executor run-job 10 TimeUnit/SECONDS)
-
-(.shutdown executor)
+(mount/stop)
 
 ;; (mount/defstate world-listener
 ;;   :start (doto (create-single-thread-scheduled-executor "world-listener")
